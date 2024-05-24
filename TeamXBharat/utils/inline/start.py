e61e13856cabe685a6705e8da9b31654dbf50c26
Honeyxcoder <160338822+HONEYXCODER@users.noreply.github.com>
2024-05-24 16:11:59 +0530

    Update start.py

--- TeamXBharat/utils/inline/start.py
+++ TeamXBharat/utils/inline/start.py
@@ -17,20 +17,5 @@ def start_panel(_):
 
 
 def private_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="lood"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
-        ],
-    ]
+    buttons =
     return buttons
